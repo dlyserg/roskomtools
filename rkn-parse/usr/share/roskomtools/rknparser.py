76bf0b1c386988b5631cac4aaad5a314a652e26b
dlyserg <kill9pid@hotmail.com>
2022-02-17 04:22:05 +0300

    Update rknparser.py

--- rkn-parse/usr/share/roskomtools/rknparser.py
+++ rkn-parse/usr/share/roskomtools/rknparser.py
@@ -7,7 +7,7 @@ re_link = r'(https?)://(([^/]*)\.([^/:\.]*))(:(\d+))?'
 def parse_ts(ts):
 	return 0
 
-def parse_registry(filename, database):
+def parse_registry(filename, database, register):
 	cursor = database.cursor()
 
 	# Очистим таблицы
@@ -25,6 +25,18 @@ def parse_registry(filename, database):
 	records = tree.xpath('//content')
 	for item in records:
 		try:
+                        if register == "SR":
+                                content_id = str(item.get('id', default = '0'))
+                                content_block_type = None
+                                content_include_time = str(item.get('includeTime', default = ''))
+                                content_urgency_type = None
+                                content_entry_type = None
+                                content_hash = item.get('hash', default = None)
+                                content_ts = None
+                                content_decision_date = None
+                                content_decision_number = None
+                                content_decision_org = None
+
 			content_id = str(item.get('id', default = '0'))
 			content_block_type = item.get('blockType', default = 'default')
 			content_ts = parse_ts(item.get('ts', default = '0'))
@@ -32,44 +44,56 @@ def parse_registry(filename, database):
 			content_urgency_type = int(item.get('urgencyType', default = 0))
 			content_entry_type = int(item.get('entryType', default = 0))
 			content_hash = item.get('hash', default = None)
-			
-			try:
-				decision = item.xpath('decision')[0]
-				content_decision_date = decision.get('date', '')
-				content_decision_number = decision.get('number', '')
-				content_decision_org = decision.get('org', '')
-			except:
-				content_decision_date = None
-				content_decision_number = None
-				content_decision_org = None
+                                data = (content_id, content_block_type, content_include_time, content_urgency_type, content_entry_type, content_hash, content_ts, content_decision_date, content_decision_number, content_decision_org, register)
+                                cursor.execute("INSERT INTO content (content_id, content_block_type, content_include_time, content_urgency_type, content_entry_type, content_hash, content_ts, content_decision_date, content_decision_number, content_decision_org, register) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+                                for domain in item.xpath('domain'):
+                                        cursor.execute("INSERT INTO domains (domain_content_id, domain_text, domain_ts, register) VALUES (?, ?, ?, ?)", (content_id, domain.text, domain.get('ts', default = 0), register))
+                                for subnet in item.xpath('ipSubnet'):
+                                        cursor.execute("INSERT INTO subnets (subnet_content_id, subnet_text, subnet_ts, register) VALUES (?, ?, ?, ?)", (content_id, subnet.text, subnet.get('ts', default = 0), register))
+                                for subnet in item.xpath('ipv6Subnet'):
+                                        cursor.execute("INSERT INTO subnetsv6 (subnet_content_id, subnet_text, subnet_ts, register) VALUES (?, ?, ?, ?)", (content_id, subnet.text, subnet.get('ts', default = 0), register))
+                        else:
+                                content_id = str(item.get('id', default = '0'))
+                                content_block_type = item.get('blockType', default = 'default')
+                                content_ts = parse_ts(item.get('ts', default = '0'))
+                                content_include_time = str(item.get('includeTime', default = ''))
+                                content_urgency_type = int(item.get('urgencyType', default = 0))
+                                content_entry_type = int(item.get('entryType', default = 0))
+                                content_hash = item.get('hash', default = None)
 
-			data = (content_id, content_block_type, content_include_time, content_urgency_type, content_entry_type, content_hash, content_ts, content_decision_date, content_decision_number, content_decision_org)
-			cursor.execute("INSERT INTO content (content_id, content_block_type, content_include_time, content_urgency_type, content_entry_type, content_hash, content_ts, content_decision_date, content_decision_number, content_decision_org) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
-
-			if content_block_type == 'default':
-				for url in item.xpath('url'):
-					cursor.execute("INSERT INTO urls (url_content_id, url_text, url_ts) VALUES (?, ?, ?)", (content_id, url.text, url.get('ts', default = 0)))
-				#for domain in item.xpath('domain'):
-				#	cursor.execute("INSERT INTO domains (domain_content_id, domain_text) VALUES (?, ?)", (content_id, domain.text))
-			elif content_block_type == 'ip':
-				for ip in item.xpath('ip'):
-					cursor.execute("INSERT INTO ips (ip_content_id, ip_text, ip_ts) VALUES (?, ?, ?)", (content_id, ip.text, ip.get('ts', 0)))
-				for subnet in item.xpath('ipSubnet'):
-					cursor.execute("INSERT INTO subnets (subnet_content_id, subnet_text, subnet_ts) VALUES (?, ?, ?)", (content_id, subnet.text, subnet.get('ts', default = 0)))
-				for ip in item.xpath('ipv6'):
-					cursor.execute("INSERT INTO ipsv6 (ip_content_id, ip_text, ip_ts) VALUES (?, ?, ?)", (content_id, ip.text, ip.get('ts', 0)))
-				for subnet in item.xpath('ipv6Subnet'):
-					cursor.execute("INSERT INTO subnetsv6 (subnet_content_id, subnet_text, subnet_ts) VALUES (?, ?, ?)", (content_id, subnet.text, subnet.get('ts', default = 0)))
-			elif content_block_type == 'domain':
-				for domain in item.xpath('domain'):
-					cursor.execute("INSERT INTO domains (domain_content_id, domain_text, domain_ts) VALUES (?, ?, ?)", (content_id, domain.text, domain.get('ts', default = 0)))
-			elif content_block_type == 'domain-mask':
-				for domain in item.xpath('domain'):
-					cursor.execute("INSERT INTO domain_masks (mask_content_id, mask_text, mask_ts) VALUES (?, ?, ?)", (content_id, domain.text, domain.get('ts', default = 0)))
-			else:
-				pass # ???
-		except:
-			continue
+                                try:
+                                        decision = item.xpath('decision')[0]
+                                        content_decision_date = decision.get('date', '')
+                                        content_decision_number = decision.get('number', '')
+                                        content_decision_org = decision.get('org', '')
+                                except:
+                                        content_decision_date = None
+                                        content_decision_number = None
+                                        content_decision_org = None
+                                data = (content_id, content_block_type, content_include_time, content_urgency_type, content_entry_type, content_hash, content_ts, content_decision_date, content_decision_number, content_decision_org, register)
+                                cursor.execute("INSERT INTO content (content_id, content_block_type, content_include_time, content_urgency_type, content_entry_type, content_hash, content_ts, content_decision_date, content_decision_number, content_decision_org, register) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+                                if content_block_type == 'default':
+                                        for url in item.xpath('url'):
+                                                cursor.execute("INSERT INTO urls (url_content_id, url_text, url_ts) VALUES (?, ?, ?)", (content_id, url.text, url.get('ts', default = 0)))
+                                elif content_block_type == 'ip':
+                                        for ip in item.xpath('ip'):
+                                                cursor.execute("INSERT INTO ips (ip_content_id, ip_text, ip_ts, register) VALUES (?, ?, ?, ?)", (content_id, ip.text, ip.get('ts', 0), register))
+                                        for subnet in item.xpath('ipSubnet'):
+                                                cursor.execute("INSERT INTO subnets (subnet_content_id, subnet_text, subnet_ts, register) VALUES (?, ?, ?, ?)", (content_id, subnet.text, subnet.get('ts', default = 0), register))
+                                        for ip in item.xpath('ipv6'):
+                                                cursor.execute("INSERT INTO ipsv6 (ip_content_id, ip_text, ip_ts, register) VALUES (?, ?, ?, ?)", (content_id, ip.text, ip.get('ts', 0), register))
+                                        for subnet in item.xpath('ipv6Subnet'):
+                                                cursor.execute("INSERT INTO subnetsv6 (subnet_content_id, subnet_text, subnet_ts, register) VALUES (?, ?, ?, ?)", (content_id, subnet.text, subnet.get('ts', default = 0), register))
+                                elif content_block_type == 'domain':
+                                        for domain in item.xpath('domain'):
+                                                cursor.execute("INSERT INTO domains (domain_content_id, domain_text, domain_ts, register) VALUES (?, ?, ?, ?)", (content_id, domain.text, domain.get('ts', default = 0), register))
+                                elif content_block_type == 'domain-mask':
+                                        for domain in item.xpath('domain'):
+                                                cursor.execute("INSERT INTO domain_masks (mask_content_id, mask_text, mask_ts) VALUES (?, ?, ?)", (content_id, domain.text, domain.get('ts', default = 0)))
+                                else:
+                                        pass # ???
+                except:
+                        continue
 
 	cursor.close()
 	database.commit()
