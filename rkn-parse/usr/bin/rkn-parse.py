e4b042ec0f6ea82ee0cc88e3fc32ffc6e153746d
dlyserg <kill9pid@hotmail.com>
2022-02-17 18:52:43 +0300

    Update rkn-parse.py

--- rkn-parse/usr/bin/rkn-parse.py
+++ rkn-parse/usr/bin/rkn-parse.py
@@ -26,42 +26,44 @@ sys.path.append('/usr/share/roskomtools')
 import rknparser
 
 # База данных
-databasecfg = config['roskomtools']['database']
-if not os.path.exists(databasecfg):
-        print("Database DATABASE doesn't exist, creating now....")
-        db = sqlite3.connect(databasecfg)
+db = sqlite3.connect(config['roskomtools']['database'])
+if db is not None:
         cursor = db.cursor()
-        logging.info("Database DATABASE doesn't exist, creating now....")
-        cursor.execute("CREATE TABLE IF NOT EXISTS content (content_id INT, content_block_type TEXT, content_include_time TEXT, content_urgency_type INT, content_entry_type INT, content_hash TEXT, content_ts INT, content_decision_date TEXT, content_decision_number TEXT, content_decision_org TEXT, register TEXT, PRIMARY KEY (content_id, register))")
-        cursor.execute("CREATE TABLE IF NOT EXISTS domains (domain_content_id INT, domain_text TEXT, domain_ts INT, register TEXT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS domain_masks (mask_content_id INT, mask_text TEXT, mask_ts INT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS urls (url_content_id INT, url_text TEXT, url_ts INT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS ips (ip_content_id INT, ip_text TEXT, ip_ts INT, register TEXT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS subnets (subnet_content_id INT, subnet_text TEXT, subnet_ts INT, register TEXT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS ipsv6 (ip_content_id INT, ip_text TEXT, ip_ts INT, register TEXT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS subnetsv6 (subnet_content_id INT, subnet_text TEXT, subnet_ts INT, register TEXT)")
-        cursor.execute("CREATE INDEX IF NOT EXISTS domain_content_id_idx ON domains (domain_content_id)")
-        cursor.execute("CREATE INDEX IF NOT EXISTS mask_content_id_idx ON domain_masks (mask_content_id)")
-        cursor.execute("CREATE INDEX IF NOT EXISTS url_content_id_idx ON urls (url_content_id)")
-        cursor.execute("CREATE INDEX IF NOT EXISTS ip_content_id_idx ON ips (ip_content_id)")
-        cursor.execute("CREATE INDEX IF NOT EXISTS subnet_content_id_idx ON subnets (subnet_content_id)")
-        print("Database DATABASES created!")
-        logging.info("Database DATABASES created!")
+        tblname = ['urls', 'domains', 'ips', 'subnets', 'ipsv6', 'subnetsv6', 'content', 'domain_masks']
+        for tableName in tblname:
+                cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name=?; ''', (tableName,))
+                if cursor.fetchone()[0] == 1 :
+                        logging.info("TABLES: %s exist. Clear." % (tableName))
+                        cursor.execute('DELETE FROM {}'.format(tableName))
+                else:
+                        if tableName == 'urls':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS urls (url_content_id INT, url_text TEXT, url_ts INT)')
+                                cursor.execute('CREATE INDEX IF NOT EXISTS url_content_id_idx ON urls (url_content_id)')
+                        elif tableName == 'domains':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS domains (domain_content_id INT, domain_text TEXT, domain_ts INT, register TEXT)')
+                                cursor.execute('CREATE INDEX IF NOT EXISTS domain_content_id_idx ON domains (domain_content_id)')
+                        elif tableName == 'ips':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS ips (ip_content_id INT, ip_text TEXT, ip_ts INT, register TEXT)')
+                                cursor.execute('CREATE INDEX IF NOT EXISTS ip_content_id_idx ON ips (ip_content_id)')
+                        elif tableName == 'subnets':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS subnets (subnet_content_id INT, subnet_text TEXT, subnet_ts INT, register TEXT)')
+                                cursor.execute('CREATE INDEX IF NOT EXISTS subnet_content_id_idx ON subnets (subnet_content_id)')
+                        elif tableName == 'ipsv6':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS ipsv6 (ip_content_id INT, ip_text TEXT, ip_ts INT, register TEXT)')
+                        elif tableName == 'subnetsv6':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS subnetsv6 (subnet_content_id INT, subnet_text TEXT, subnet_ts INT, register TEXT)')
+                        elif tableName == 'content':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS content (content_id INT, content_block_type TEXT, content_include_time TEXT, content_urgency_type INT, content_entry_type INT, content_hash TEXT, content_ts INT, content_decision_date TEXT, content_decision_number TEXT, content_decision_org TEXT, register TEXT, PRIMARY KEY (content_id, register))')
+                        elif tableName == 'domain_masks':
+                                cursor.execute('CREATE TABLE IF NOT EXISTS domain_masks (mask_content_id INT, mask_text TEXT, mask_ts INT)')
+                                cursor.execute('CREATE INDEX IF NOT EXISTS mask_content_id_idx ON domain_masks (mask_content_id)')
+                        else:
+                                pass
+        cursor.close()
+        db.commit()
 else:
-        print("DATABASE exists, clear TABLES")
-        db = sqlite3.connect(databasecfg)
-        cursor = db.cursor()
-        logging.info("Clear TABLES")
-        cursor.execute("DELETE FROM urls")
-        cursor.execute("DELETE FROM domains")
-        cursor.execute("DELETE FROM ips")
-        cursor.execute("DELETE FROM subnets")
-        cursor.execute("DELETE FROM ipsv6")
-        cursor.execute("DELETE FROM subnetsv6")
-        cursor.execute("DELETE FROM content")
-        cursor.execute("DELETE FROM domain_masks")
-cursor.close()
-db.commit()
+        print("Error! cannot create the database connection.")
+        logging.error("Error! cannot create the database connection.")
 
 print("Parsing the registry...")
 logging.info("Start parsing the registry...")
