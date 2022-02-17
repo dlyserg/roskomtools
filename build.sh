15c2f80cc8b405787b1f1f7724ad431ceeb2cf9a
dlyserg <kill9pid@hotmail.com>
2022-02-17 04:25:54 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fakeroot dpkg-deb --build rkn-common
-fakeroot dpkg-deb --build rkn-load
-fakeroot dpkg-deb --build rkn-check
-fakeroot dpkg-deb --build rkn-parse
-fakeroot dpkg-deb --build rkn-cronjobs
-fakeroot dpkg-deb --build rkn-status
